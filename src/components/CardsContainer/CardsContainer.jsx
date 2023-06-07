b984c05dad6b1e0ebc91a96e6a14b36766a59b59
Lucass2160 <lucas-cali@hotmail.com>
2023-06-07 16:12:54 -0300

    paginado

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { Paginado } from "../Paginado/Paginado";
 import Card from "../Card/Card";
 //import { Link } from "react-router-dom";
 import style from "./CardsContainer.module.css";
@@ -7,29 +8,19 @@ import style from "./CardsContainer.module.css";
 const CardsContainer = () => {
   const products = useSelector((state) => state.products);
 
-  //Paginado
-  const [currentPage, setCurrentPage] = useState(0);
-  const productsPerPage = 6;
-  const filterProducts = products.slice(
-    currentPage,
-    currentPage + productsPerPage
-  );
-//  const totalPages = Math.ceil(products.length / productsPerPage);
+  const [pagina, setPagina] = useState(1);
+  const [porPagina, setPorPagina] = useState(6);
+  const maximo = products.length / porPagina;
 
-  const nextPage = () => {
-    document.documentElement.scrollTop = 100;
-    setCurrentPage(currentPage + 6);
-  };
-  const prevPage = () => {
-    document.documentElement.scrollTop = 100;
-    if (currentPage > 0) setCurrentPage(currentPage - 6);
-  };
+  console.log(maximo);
 
   return (
     <div>
       <div className={style.container}>
-        {console.log(filterProducts, "esto es filterProducts")}
-        {filterProducts?.map((product) => {
+        {products?.slice(
+          (pagina - 1) * porPagina,
+          (pagina - 1) * porPagina + porPagina
+        ).map((product) => {
           return (
             <>
               <div>
@@ -45,16 +36,8 @@ const CardsContainer = () => {
           );
         })}
       </div>
-      <div>
-        <div className="clicks">
-          <button className={style.paginado} onClick={prevPage}>
-            Anterior
-          </button>
-          <button className={style.paginado} onClick={nextPage}>
-            Siguiente
-          </button>
-        </div>
-      </div>
+
+      <Paginado pagina={pagina} setPagina={setPagina} maximo={maximo} />
     </div>
   );
 };

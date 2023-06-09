8f918beca9dfbada32f830fd6754e9895418ff00
miamartinezfe <miguelshaddix95@hotmail.com>
2023-06-08 23:30:26 -0500

    fron/deploy
    added some eslint lines to avoid warnings on npm start

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -28,7 +28,7 @@ const SignUp = () => {
     const validationErrors = validateUser(user);
     if (Object.keys(validationErrors).length === 0) {
       try {
-        const response = await fetch("http://localhost:3001/users/signup", {
+        const response = await fetch("/users/signup", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",

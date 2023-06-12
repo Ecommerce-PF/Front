0e0930b570f1efd236248734b2ceace050e7a91c
Lucass2160 <lucas-cali@hotmail.com>
2023-06-12 09:59:46 -0300

    todos los cambios de css

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 import { getDetail, addCart } from "../../redux/actions/actions.js";
 import { FaCartArrowDown, FaArrowLeft } from "react-icons/fa";
@@ -25,82 +25,74 @@ export default function Detail() {
     localStorage.setItem("carritoLS", JSON.stringify(listaCart));
 
     Swal.fire({
-      icon: 'success',
-      title: 'Su producto se a agregado al carrito!!',
+      icon: "success",
+      title: "Su producto se a agregado al carrito!!",
       showConfirmButton: false,
-      timer: 1500
-    })
-  }
+      timer: 1500,
+    });
+  };
 
   useEffect(() => {
     dispatch(getDetail(id));
   }, [dispatch, id]);
 
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.productImg}>
-        <h3>{state?.name}</h3>
-        <div className={styles.img}>
-          <img
-            src={state?.image}
-            alt={state?.name}
-            className={styles.imgProducto}
-          />
-        </div>
-        <div className={styles.buyNow}>
-          <h3>BUY NOW</h3>
-          <h3>${state?.price}</h3>
-        </div>
-        <div>
-          <label htmlFor="color">Color:</label>
-          <select type="select" name="color">
-            <option>None</option>
-            {state?.color &&
-              state.color.map((e) => (
-                <option
-                  className={styles.color}
-                  name={e.ColorName}
-                  key={e.ColorName}
-                >
-                  {e.ColorName}
-                </option>
-              ))}
-          </select>
-          <div>
-            <label htmlFor="color">Size:</label>
-            <button className={styles.size}>S</button>
-            <button className={styles.size}>M</button>
-            <button className={styles.size}>L</button>
+    <div className={styles.back}>
+      <div className={styles.mainContainer}>
+        <div className={styles.productImg}>
+          <h3>{state?.name}</h3>
+          <div className={styles.img}>
+            <img
+              src={state?.image}
+              alt={state?.name}
+              className={styles.imgProducto}
+            />
+          </div>
+          <div className={styles.buyNow}>
+            <h1>${state?.price}</h1>
+          </div>
+
+          <div className={styles.containerSA}>
+            <label htmlFor="color">Color:</label>
+            <select className={styles.buttonSelect} type="select" name="color">
+              <option className={styles.option}>None</option>
+              {state?.color &&
+                state.color.map((e) => (
+                  <option
+                    className={styles.option}
+                    name={e.ColorName}
+                    key={e.ColorName}
+                  >
+                    {e.ColorName}
+                  </option>
+                ))}
+            </select>
+            <div>
+              <label htmlFor="color">Size:</label>
+              <button className={styles.size}>S</button>
+              <button className={styles.size}>M</button>
+              <button className={styles.size}>L</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.details}>
-        <div>
-          <div dangerouslySetInnerHTML={{ __html: state?.description }}></div>
-        </div>
-        <div className={styles.cart}>
-          <button onClick={handleAddCart}>
-            Add to Cart{" "}
-            <FaCartArrowDown className={styles.icon}></FaCartArrowDown>
-          </button>
-          <NavLink to="/home">
-            <button className={styles.bttnBack}>
-              Back <FaArrowLeft className={styles.icon}></FaArrowLeft>
+        <div className={styles.details}>
+          <div className={styles.description}>
+            <div className={styles.textss}
+              dangerouslySetInnerHTML={{ __html: state?.description }}
+            ></div>
+          </div>
+          <div className={styles.cart}>
+            <button className={styles.button} onClick={handleAddCart}>
+              Add to Cart{" "}
+              <FaCartArrowDown className={styles.icon}></FaCartArrowDown>
             </button>
-          </NavLink>
-
-          <h3>Quantity:</h3>
-          <form action="">
-            <label htmlFor="quantity">Quantity:</label>
-            <input type="number" name="quantity" id="quantity" min="1" />
-            <button type="submit">
-                <FaCartArrowDown className={styles.icon} />
-            </button>
-
-
-            
-        </form>
+            <NavLink to="/home">
+              <button className={styles.button}>
+                Back <FaArrowLeft className={styles.icon}></FaArrowLeft>
+              </button>
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>

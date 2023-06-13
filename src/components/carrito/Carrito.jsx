4ce3bcf24f3c62c58a242aeaccf2b3211caf1f79
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-06-13 17:15:15 -0600

    Corregimos funcion en el carrito para que quede funcional

--- src/components/carrito/Carrito.jsx
+++ src/components/carrito/Carrito.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from '../Nav/Nav.jsx';
+import axios from "axios";
 import { FaSadTear } from 'react-icons/fa';
 import CartProduct from "./cartProduct/CartProduct.jsx";
 import { NavLink } from "react-router-dom";
@@ -10,14 +11,15 @@ import styles from "./carrito.module.css";
 
 export default function Carrito() {
 
-    const cart = JSON.parse(localStorage.getItem("carritoLS"));
+  const cart = JSON.parse(localStorage.getItem("carritoLS"));
+  const idUser = localStorage.getItem("id");
 
-    if (cart !== null && cart.length > 0) {
-        var precioTotal = 0;
-        for (let i = 0; i < cart.length; i++) {
-            precioTotal += cart[i].price;
-        }
+  if (cart !== null && cart.length > 0) {
+    var precioTotal = 0;
+    for (let i = 0; i < cart.length; i++) {
+      precioTotal += cart[i].price;
     }
+  }
 
   function eliminarObjetosRepetidos(array) {
     var objetosUnicos = [];
@@ -35,43 +37,61 @@ export default function Carrito() {
     return objetosUnicos;
   }
 
-    const productosUnicos = eliminarObjetosRepetidos(cart);
+  const productosUnicos = eliminarObjetosRepetidos(cart);
 
-    return (
-        <section>
-        <section className={styles.componentCart} >
-            <Nav />
-            {cart === null || cart.length > 0 ?
-                <div className={styles.containerCart}>
-                    {productosUnicos.map(product => {
-                        return (<CartProduct key={product.id} product={product} />)
-                    })}
+  const funcionPago = async () => {
+    var arrProducts = [];
+    for (let i = 0; i < cart.length; i++) {
+      arrProducts.push({
+        id: cart[i].id,
+        quantity: cart[i].quantity,
+      });
+    }
+    const body = {
+      "products": arrProducts,
+      "userId": idUser
+    }
+    const newOrder = await axios.post('http://localhost:3001/payment/create-order', body);
 
-                    <div className={styles.carritoTotalPrecio} >
-                        <h3>Total del carrito: {precioTotal}</h3> <button onClick={() => alert("En producción")} >Proceder al pago</button>
-                    </div>
-                </div> :
-                <section className={styles.emptyCart}>
-                    <div className={styles.cart}></div>
-                    <div className={styles.cartDescription}>
-                        <h2>
-                            Tu carrito de compras está vacio! <FaSadTear></FaSadTear>
-                        </h2>
-                        <NavLink to='/home'>
-                            <button>Buscar articulos</button>
-                        </NavLink>
-                        <button onClick={() => window.location.reload()} > <TfiReload /> </button>
-                    </div>
-                </section>
-            }
+    console.log(newOrder.data);
+    window.location.replace(newOrder.data);
+  }
 
-          <div className={styles.types}>
-        <Link to="/home">
-          <button className={styles.button}>Back</button>
-        </Link>
-      </div>
-        </section>
-      
+  return (
+    <section>
+      <section className={styles.componentCart} >
+        <Nav />
+        {cart === null || cart.length > 0 ?
+          <div className={styles.containerCart}>
+            {productosUnicos.map(product => {
+              return (<CartProduct key={product.id} product={product} />)
+            })}
+
+            <div className={styles.carritoTotalPrecio} >
+              <h3>Total del carrito: {precioTotal}</h3> <button onClick={funcionPago} >Proceder al pago</button>
+            </div>
+          </div> :
+          <section className={styles.emptyCart}>
+            <div className={styles.cart}></div>
+            <div className={styles.cartDescription}>
+              <h2>
+                Tu carrito de compras está vacio! <FaSadTear></FaSadTear>
+              </h2>
+              <NavLink to='/home'>
+                <button>Buscar articulos</button>
+              </NavLink>
+              <button onClick={() => window.location.reload()} > <TfiReload /> </button>
+            </div>
+          </section>
+        }
+
+        <div className={styles.types}>
+          <Link to="/home">
+            <button className={styles.button}>Back</button>
+          </Link>
+        </div>
+      </section>
+
     </section>
   );
 }

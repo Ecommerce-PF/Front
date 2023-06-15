ef459923d64d6f32015f23445cfe19ca8c5ebe20
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-06-15 17:35:31 -0600

    Details optimizado

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,86 +1,89 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
 import Swal from "sweetalert2";
 import { getDetail, addCart } from "../../redux/actions/actions.js";
 import { FaCartArrowDown, FaArrowLeft } from "react-icons/fa";
-// import { useState } from "react";
-
+import { MdOutlineArrowForwardIos, MdOutlineArrowBackIos } from "react-icons/md"
 import styles from "./detail.module.css";
 
-export default function Detail() {
+function Detail() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const state = useSelector((state) => state.productDetail);
 
+  const [coloresPrt, setColoresPrt] = useState([]);
+  const [numberImg, setNumberImg] = useState(0);
+  const [colorProductImg, setColorProductImg] = useState('');
+
   const handleAddCart = () => {
     dispatch(addCart(state));
-    let listaCart = JSON.parse(localStorage.getItem("carritoLS"));
-    if (listaCart === null) {
-      listaCart = [];
-    } else {
-      for (var i = 0; i < listaCart.length; i++) {
-        if (listaCart[i].id === state.id) {
-          return (Swal.fire({
-            icon: 'error',
-            title: 'To producto ya se encuentra en Carrito!',
-            showConfirmButton: false,
-            timer: 1500
-          }))
-        }
-      }
 
-      listaCart.push({
-        ...state,
-        quantity: 1
-      });
+    const listaCart = JSON.parse(localStorage.getItem("carritoLS")) || [];
+
+    for (let i = 0; i < listaCart.length; i++) {
+      if (listaCart[i].id === state.id) {
+        return Swal.fire({
+          icon: 'error',
+          title: '¡El producto ya se encuentra en el carrito!',
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
     }
+
+    listaCart.push({
+      ...state,
+      quantity: 1
+    });
+
     localStorage.setItem("carritoLS", JSON.stringify(listaCart));
 
     Swal.fire({
       icon: "success",
-      title: "Su producto se ha agregado al carrito!!",
+      title: "¡Su producto se ha agregado al carrito!",
       showConfirmButton: false,
       timer: 1500,
     });
   };
 
+  const handleChange = (e) => {
+    if (e.target.value === 'None') {
+      setColoresPrt([]);
+    } else {
+      setColorProductImg(e.target.value);
+      setColoresPrt(clrPrdct(e.target.value));
+    }
+  };
+
+  const clrPrdct = (valorBuscado) => {
+    for (let i = 0; i < state.color.length; i++) {
+      if (state.color[i].ColorName === valorBuscado) {
+        return state.color[i].ProductImages;
+      }
+    }
+    return null;
+  };
+
+  const changeLeft = () => {
+    if (numberImg > 0) {
+      setNumberImg(numberImg - 1);
+    } else {
+      setNumberImg(coloresPrt.length - 1);
+    }
+  };
+
+  const changeRigth = () => {
+    if (numberImg < coloresPrt.length - 1) {
+      setNumberImg(numberImg + 1);
+    } else {
+      setNumberImg(0);
+    }
+  };
+
   useEffect(() => {
     dispatch(getDetail(id));
   }, [dispatch, id]);
-
-  //Logica de galeria img
-  const [colorProductImg, setColorProductImg] = useState('');
-  const [coloresPrt, setColoresPrt] = useState([]);
-
-  function handleChange(e) {
-    console.log(e.target.value)
-    if (e.target.value === 'None') {
-      setColoresPrt([])
-    } else {
-      (setColorProductImg(e.target.value));
-      setColoresPrt(clrPrdct(e.target.value));
-    }
-  }
-
-  // console.log(coloresPrt);
-
-  const clrPrdct = (valorBuscado) => {
-    var arrayColorEncontrado = null;
-
-    for (var i = 0; i < state.color.length; i++) {
-      if (state.color[i].ColorName === valorBuscado) {
-        arrayColorEncontrado = state.color[i].ProductImages;
-        // console.log(arrayColorEncontrado);
-        return arrayColorEncontrado;
-      }
-    }
-  }
-
-  useEffect(() => {
-    // setColorProductImg()
-  }, [])
 
   return (
     <div className={styles.back}>
@@ -88,18 +91,23 @@ export default function Detail() {
         <div className={styles.productImg}>
           <h3>{state?.name}</h3>
           <div className={styles.img}>
-
-            {/* Render de imagen  si hay imagenes en e array renderizamos el array si no, mandamos la imagen que tenemos.*/}
-            {/* {!!coloresPrt.length ? */}
-            {/* <NoTransitionExample coloresPrt={coloresPrt} /> : */}
-            <img
-              src={state?.image}
-              alt={state?.name}
-              className={styles.imgProducto}
-            />
-            {/* } */}
-
-
+            {!!coloresPrt.length ? (
+              <div>
+                <button onClick={changeLeft}><MdOutlineArrowBackIos /></button>
+                <img
+                  src={coloresPrt[numberImg]}
+                  alt={state?.name}
+                  className={styles.imgProducto}
+                />
+                <button onClick={changeRigth}><MdOutlineArrowForwardIos /></button>
+              </div>
+            ) : (
+              <img
+                src={state?.image}
+                alt={state?.name}
+                className={styles.imgProducto}
+              />
+            )}
           </div>
           <div className={styles.buyNow}>
             <h1>${state?.price}</h1>
@@ -107,18 +115,13 @@ export default function Detail() {
 
           <div className={styles.containerSA}>
             <label htmlFor="color">Color:</label>
-            <select className={styles.buttonSelect} type="select" name="color">
+            <select className={styles.buttonSelect} type="select" name="color" onChange={handleChange}>
               <option className={styles.option}>None</option>
-              {state?.color &&
-                state.color.map((e) => (
-                  <option
-                    className={styles.option}
-                    name={e.ColorName}
-                    key={e.ColorName}
-                  >
-                    {e.ColorName}
-                  </option>
-                ))}
+              {state?.color && state.color.map((e) => (
+                <option className={styles.option} name={e.ColorName} key={e.ColorName}>
+                  {e.ColorName}
+                </option>
+              ))}
             </select>
             <div>
               <label htmlFor="color">Size:</label>
@@ -131,14 +134,14 @@ export default function Detail() {
 
         <div className={styles.details}>
           <div className={styles.description}>
-            <div className={styles.textss}
+            <div
+              className={styles.textss}
               dangerouslySetInnerHTML={{ __html: state?.description }}
             ></div>
           </div>
           <div className={styles.cart}>
             <button className={styles.button} onClick={handleAddCart}>
-              Add to Cart{" "}
-              <FaCartArrowDown className={styles.icon}></FaCartArrowDown>
+              Add to Cart <FaCartArrowDown className={styles.icon}></FaCartArrowDown>
             </button>
             <NavLink to="/home">
               <button className={styles.button}>
@@ -151,3 +154,5 @@ export default function Detail() {
     </div>
   );
 }
+
+export default Detail;

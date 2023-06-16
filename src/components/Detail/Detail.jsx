8d542a5fbf1aa3d7fc0c43efeee1207490b6525f
Lucass2160 <lucas-cali@hotmail.com>
2023-06-16 01:03:04 -0300

    esto anda

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -3,7 +3,11 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import Swal from "sweetalert2";
-import { getDetail, addCart } from "../../redux/actions/actions.js";
+import {
+  getDetail,
+  addCart,
+  getUserById,
+} from "../../redux/actions/actions.js";
 import { FaCartArrowDown, FaArrowLeft } from "react-icons/fa";
 import { useState } from "react";
 import axios from "axios";
@@ -15,6 +19,18 @@ export default function Detail() {
   const { id } = useParams();
   const state = useSelector((state) => state.productDetail);
   const idUser = useSelector((state) => state.idUsuario);
+  const user = useSelector((state) => state.userId);
+
+  if (user.length === 0) {
+    // No hacer nada
+  } else {
+    localStorage.setItem("users", user.name);
+  }
+  const userOnline = localStorage.getItem("users");
+
+  console.log(userOnline, "userOnline");
+
+  const [comments, setComments] = useState([]);
 
   if (idUser.length === 0) {
     // No hacer nada
@@ -30,7 +46,7 @@ export default function Detail() {
   const [form, setForm] = useState({
     review: "",
     rating: 5,
-    date: fecha.fecha,
+    date: fecha.fecha + "",
     UserId: idUsers,
     ClotheId: id,
   });
@@ -45,7 +61,22 @@ export default function Detail() {
     });
   };
 
-  const submitHandler = async () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/products/${id}/reviews`);
+        console.log(response.data, "response");
+        setComments(response.data); // Asignar los comentarios a la variable 'comments'
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
     try {
       await axios.post(`/products/${id}/reviews`, JSON.stringify(form), {
         headers: {
@@ -57,6 +88,8 @@ export default function Detail() {
       alert(err);
     }
   };
+
+  console.log(form);
 
   const handleAddCart = () => {
     dispatch(addCart(state));
@@ -92,6 +125,7 @@ export default function Detail() {
 
   useEffect(() => {
     dispatch(getDetail(id));
+    dispatch(getUserById(idUsers));
   }, [dispatch, id]);
 
   return (
@@ -162,43 +196,74 @@ export default function Detail() {
       </div>
 
       <div className={styles.back2}>
-        <div className={styles.containerComentarios}>
-          <h2>Comentario de NOMBRE</h2>
-          <p>Me gusto mucho...</p>
-        </div>
-
-        <h1>Agregue su comentario</h1>
-        <form action="" onSubmit={submitHandler}>
-          <div className={styles.enviarBut}>
-            <label htmlFor="">Review</label>
-
-            <input
-              name="review"
-              value={form.review}
-              onChange={hanleChange}
-              className={styles.inputC}
-              type="text"
-            ></input>
-
-            <label htmlFor="">Rating</label>
-
-            <input
-              name="rating"
-              value={form.rating}
-              onChange={hanleChange}
-              className={styles.inputC}
-              type="range"
-              min="1"
-              max="5"
-              step="1"
-            />
-
-            <span>{form.rating}</span>
-
-            <button type="submit">Enviar</button>
+        {comments.map((comment) => (
+          <div className={styles.reviewss} key={comment.id}>
+            <p className={styles.info}>Review: {comment.review}</p>
+            <p className={styles.info}>Rating:</p>
+            {Array.from({ length: comment.rating }).map((_, index) => (
+              <svg
+                className={styles.stars}
+                key={index}
+                width="0px"
+                height="20px"
+                viewBox="0 0 25 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M13 4L15.2747 9.8691L21.5595 10.2188L16.6806 14.1959L18.2901 20.2812L13 16.87L7.70993 20.2812L9.31941 14.1959L4.44049 10.2188L10.7253 9.8691L13 4Z"
+                  stroke="#121923"
+                  strokeWidth="1.2"
+                />
+              </svg>
+            ))}
+            <p className={styles.info}>
+              Hour:
+              <p className={styles.info}>
+                {new Date(comment.date).toLocaleString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  day: "numeric",
+                  month: "numeric",
+                  year: "numeric",
+                })}
+              </p>
+            </p>
           </div>
-        </form>
+        ))}
       </div>
+
+      <h1>Agregue su comentario</h1>
+      <form action="" onSubmit={submitHandler}>
+        <div className={styles.enviarBut}>
+          <label htmlFor="">Review</label>
+
+          <input
+            name="review"
+            value={form.review}
+            onChange={hanleChange}
+            className={styles.inputC}
+            type="text"
+          ></input>
+
+          <label htmlFor="">Rating</label>
+
+          <input
+            name="rating"
+            value={form.rating}
+            onChange={hanleChange}
+            className={styles.inputC}
+            type="range"
+            min="1"
+            max="5"
+            step="1"
+          />
+
+          <span>{form.rating}</span>
+
+          <button type="submit">Enviar</button>
+        </div>
+      </form>
     </div>
   );
 }

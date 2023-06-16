49457018674340ac0b904cdcba77155300011acf
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-06-16 01:18:15 -0600

    conflictos

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -111,155 +111,155 @@ function Detail() {
         }
       }
 
-    listaCart.push({
-      ...state,
-      quantity: 1,
-    });
+      listaCart.push({
+        ...state,
+        quantity: 1,
+      });
 
-    localStorage.setItem("carritoLS", JSON.stringify(listaCart));
+      localStorage.setItem("carritoLS", JSON.stringify(listaCart));
 
-    Swal.fire({
-      icon: "success",
-      title: "¡Su producto se ha agregado al carrito!",
-      showConfirmButton: false,
-      timer: 1500,
-    });
-  };
+      Swal.fire({
+        icon: "success",
+        title: "¡Su producto se ha agregado al carrito!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    };
 
-  useEffect(() => {
-    dispatch(getDetail(id));
-    dispatch(getUserById(idUsers));
-  }, [dispatch, id]);
+    useEffect(() => {
+      dispatch(getDetail(id));
+      dispatch(getUserById(idUsers));
+    }, [dispatch, id]);
 
-  //Logica de galeria img
-  const [colorProductImg, setColorProductImg] = useState('');
-  const [coloresPrt, setColoresPrt] = useState([]);
+    //Logica de galeria img
+    const [colorProductImg, setColorProductImg] = useState('');
+    const [coloresPrt, setColoresPrt] = useState([]);
 
-  function handleChange(e) {
-    console.log(e.target.value)
-  const handleChange = (e) => {
-    if (e.target.value === 'None') {
-      setColoresPrt([]);
-    } else {
-      setColorProductImg(e.target.value);
-      setColoresPrt(clrPrdct(e.target.value));
-      setSizeArr(findArrSize(e.target.value));
-    }
-  };
+    function handleChange(e) {
+      console.log(e.target.value)
+      const handleChange = (e) => {
+        if (e.target.value === 'None') {
+          setColoresPrt([]);
+        } else {
+          setColorProductImg(e.target.value);
+          setColoresPrt(clrPrdct(e.target.value));
+          setSizeArr(findArrSize(e.target.value));
+        }
+      };
 
-  const clrPrdct = (valorBuscado) => {
-    for (let i = 0; i < state.color.length; i++) {
-      if (state.color[i].ColorName === valorBuscado) {
-        return state.color[i].ProductImages;
+      const clrPrdct = (valorBuscado) => {
+        for (let i = 0; i < state.color.length; i++) {
+          if (state.color[i].ColorName === valorBuscado) {
+            return state.color[i].ProductImages;
+          }
+        }
+        return null;
+      };
+
+      const findArrSize = (valorBuscado) => {
+        for (let i = 0; i < state.color.length; i++) {
+          if (state.color[i].ColorName === valorBuscado) {
+            return state.color[i].Sizes;
+          }
+        }
+        return null;
       }
-    }
-    return null;
-  };
 
-  const findArrSize = (valorBuscado) =>{
-    for (let i = 0; i < state.color.length; i++) {
-      if (state.color[i].ColorName === valorBuscado) {
-        return state.color[i].Sizes;
-      }
-    }
-    return null;
-  }
+      const changeLeft = () => {
+        if (numberImg > 0) {
+          setNumberImg(numberImg - 1);
+        } else {
+          setNumberImg(coloresPrt.length - 1);
+        }
+      };
 
-  const changeLeft = () => {
-    if (numberImg > 0) {
-      setNumberImg(numberImg - 1);
-    } else {
-      setNumberImg(coloresPrt.length - 1);
-    }
-  };
+      const changeRigth = () => {
+        if (numberImg < coloresPrt.length - 1) {
+          setNumberImg(numberImg + 1);
+        } else {
+          setNumberImg(0);
+        }
+      };
 
-  const changeRigth = () => {
-    if (numberImg < coloresPrt.length - 1) {
-      setNumberImg(numberImg + 1);
-    } else {
-      setNumberImg(0);
-    }
-  };
+      console.log(sizesArr);
 
-  console.log(sizesArr);
+      useEffect(() => {
+        dispatch(getDetail(id));
+      }, [dispatch, id]);
 
-  useEffect(() => {
-    dispatch(getDetail(id));
-  }, [dispatch, id]);
-
-  return (
-    <div className={styles.back}>
-      <div className={styles.mainContainer}>
-        <div className={styles.productImg}>
-          <h3>{state?.name}</h3>
-          <div className={styles.img}>
-            {!!coloresPrt.length ? (
-              <div className={styles.divGaleryImg}>
-                <button onClick={changeLeft} className={styles.bttnArrow} ><MdOutlineArrowBackIos /></button>
-                <img
-                  src={coloresPrt[numberImg]}
-                  alt={state?.name}
-                  className={styles.imgProducto}
-                />
-                <button onClick={changeRigth} className={styles.bttnArrow} ><MdOutlineArrowForwardIos /></button>
+      return (
+        <div className={styles.back}>
+          <div className={styles.mainContainer}>
+            <div className={styles.productImg}>
+              <h3>{state?.name}</h3>
+              <div className={styles.img}>
+                {!!coloresPrt.length ? (
+                  <div className={styles.divGaleryImg}>
+                    <button onClick={changeLeft} className={styles.bttnArrow} ><MdOutlineArrowBackIos /></button>
+                    <img
+                      src={coloresPrt[numberImg]}
+                      alt={state?.name}
+                      className={styles.imgProducto}
+                    />
+                    <button onClick={changeRigth} className={styles.bttnArrow} ><MdOutlineArrowForwardIos /></button>
+                  </div>
+                ) : (
+                  <img
+                    src={state?.image}
+                    alt={state?.name}
+                    className={styles.imgProducto}
+                  />
+                )}
               </div>
-            ) : (
-              <img
-                src={state?.image}
-                alt={state?.name}
-                className={styles.imgProducto}
-              />
-            )}
-          </div>
-          <div className={styles.buyNow}>
-            <h1>${state?.price}</h1>
-          </div>
+              <div className={styles.buyNow}>
+                <h1>${state?.price}</h1>
+              </div>
 
-          <div className={styles.containerSA}>
-            <label htmlFor="color">Color:</label>
-            <select className={styles.buttonSelect} type="select" name="color">
-              <option className={styles.option}>None</option>
-              {state?.color &&
-                state.color.map((e) => (
-                  <option
-                    className={styles.option}
-                    name={e.ColorName}
-                    key={e.ColorName}
-                  >
-                    {e.ColorName}
-                  </option>
-                ))}
-            </select>
-            <div>
-              <label htmlFor="color">Size:</label>
-              <button className={styles.size}>S</button>
-              <button className={styles.size}>M</button>
-              <button className={styles.size}>L</button>
+              <div className={styles.containerSA}>
+                <label htmlFor="color">Color:</label>
+                <select className={styles.buttonSelect} type="select" name="color">
+                  <option className={styles.option}>None</option>
+                  {state?.color &&
+                    state.color.map((e) => (
+                      <option
+                        className={styles.option}
+                        name={e.ColorName}
+                        key={e.ColorName}
+                      >
+                        {e.ColorName}
+                      </option>
+                    ))}
+                </select>
+                <div>
+                  <label htmlFor="color">Size:</label>
+                  <button className={styles.size}>S</button>
+                  <button className={styles.size}>M</button>
+                  <button className={styles.size}>L</button>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.details}>
+              <div className={styles.description}>
+                <div className={styles.textss}
+                  dangerouslySetInnerHTML={{ __html: state?.description }}
+                ></div>
+              </div>
+              <div className={styles.cart}>
+                <button className={styles.button} onClick={handleAddCart}>
+                  Add to Cart{" "}
+                  <FaCartArrowDown className={styles.icon}></FaCartArrowDown>
+                </button>
+                <NavLink to="/home">
+                  <button className={styles.button}>
+                    Back <FaArrowLeft className={styles.icon}></FaArrowLeft>
+                  </button>
+                </NavLink>
+              </div>
             </div>
           </div>
         </div>
+      );
+    }
 
-        <div className={styles.details}>
-          <div className={styles.description}>
-            <div className={styles.textss}
-              dangerouslySetInnerHTML={{ __html: state?.description }}
-            ></div>
-          </div>
-          <div className={styles.cart}>
-            <button className={styles.button} onClick={handleAddCart}>
-              Add to Cart{" "}
-              <FaCartArrowDown className={styles.icon}></FaCartArrowDown>
-            </button>
-            <NavLink to="/home">
-              <button className={styles.button}>
-                Back <FaArrowLeft className={styles.icon}></FaArrowLeft>
-              </button>
-            </NavLink>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Detail;
+    export default Detail;

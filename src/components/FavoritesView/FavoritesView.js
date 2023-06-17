163b4517418e945e08e0a19a4e2a77af956019c5
Lucass2160 <lucas-cali@hotmail.com>
2023-06-16 21:05:05 -0300

    a

--- src/components/FavoritesView/FavoritesView.js
+++ src/components/FavoritesView/FavoritesView.js
@@ -1,61 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { addFavorite, deleteFavorite } from "../../redux/actions/actions";
+import {
+  addFavorite,
+  deleteFavorite,
+  getUser,
+  deleteProduct,
+} from "../../redux/actions/actions";
 import Card from "../Card/Card";
-import { getUser, deleteProduct } from "../../redux/actions/actions";
 import styles from "./favorite.module.css";
+import axios from "axios";
 
-// crear un boton de back para regresar a favoritos
 const FavoritesView = () => {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const [favProducts, setFavProducts] = useState([]);
-  const myFavorites = useSelector((state) => state.myFavorites);
+  const { id } = useParams();
+
+  const [favoriteProducts, setFavoriteProducts] = useState([]);
 
   useEffect(() => {
-    dispatch(getUser(user.id));
-  }, []);
+    const fetchFavoriteProducts = async () => {
+      try {
+        const response = await axios.get(`/whishListProduct/${id}`);
+        setFavoriteProducts(response.data.Clothes);
+      } catch (error) {
+        console.error("Error al obtener los productos favoritos", error);
+      }
+    };
 
-  useEffect(() => {
-    setFavProducts(user.products);
-  }, [user]);
+    fetchFavoriteProducts();
+  }, [id]);
 
-  useEffect(() => {
-    // Actualiza la lista de productos favoritos cuando el estado del usuario cambia
-  }, [myFavorites]);
-
-  const handleAddFavorite = () => {
-    // Agrega el producto a la lista de favoritos del usuario
-    dispatch(addFavorite());
-  };
-
-  const handleDeleteFavorite = (id) => {
-    dispatch(deleteFavorite(id));
-  };
-
-  const handleDelete = (id) => {
-    dispatch(deleteProduct(id));
-  };
-  // dar etiqutas de estilos para style.module.css
   return (
     <div className={styles.container_fav}>
       <h1 className={styles.title_fav}>Productos Favoritos</h1>
-      {myFavorites.map((favorite) => {
-        return (
-          <Card
-            key={favorite.id}
-            id={favorite.id}
-            name={favorite.name}
-            image={favorite.image}
-            price={favorite.price}
-          >
-            <button onClick={() => handleDeleteFavorite(favorite.id)}>
-              Eliminar
-            </button>
-          </Card>
-        );
-      })}
+      {favoriteProducts.map((favorite) => (
+        <Card
+          key={favorite.id}
+          id={favorite.id}
+          name={favorite.name}
+          image={favorite.image}
+          price={favorite.price}
+        >
+          <button onClick={() => handleDeleteFavorite(favorite.id)}>
+            Eliminar
+          </button>
+        </Card>
+      ))}
       <NavLink to="/home" className={styles.navlink}>
         <button>Back</button>
       </NavLink>
